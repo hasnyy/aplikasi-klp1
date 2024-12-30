6e4f89d40d5405ccf162b3fb132403bf84712aa4
kelompok1 <hasniawati979@gmail.com>
2024-12-30 16:47:51 +0700

    versi 4 : tombol warna dan pengambilan warna catatan

--- app/src/main/java/com/example/kelompok1/Home.java
+++ app/src/main/java/com/example/kelompok1/Home.java
@@ -10,12 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Home extends AppCompatActivity {
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        ImageView btnTambah=findViewById(R.id.btnTambah);
+        ImageView btnTambah = findViewById(R.id.btnTambah);
         btnTambah.setOnClickListener(v -> {
             startActivity(new Intent(this, MainActivity.class));
         });
@@ -24,13 +25,6 @@ public class Home extends AppCompatActivity {
         GridView gridview=findViewById(R.id.gridCatatan);
 
         List<ModelCatatan> modelcatatan=new ArrayList<>();
-        modelcatatan.add(new ModelCatatan("Catatn1","nama saya hasni","29/12/2024"));
-        modelcatatan.add(new ModelCatatan("Catatn2","nama saya fila","29/12/2024"));
-        modelcatatan.add(new ModelCatatan("Catatn3","nama saya asya","29/12/2024"));
-        modelcatatan.add(new ModelCatatan("Catatn4","nama saya abu","29/12/2024"));
-        modelcatatan.add(new ModelCatatan("Catatn5","nama saya anto","29/12/2024"));
-        modelcatatan.add(new ModelCatatan("Catatn6","nama saya labibi","29/12/2024"));
-        modelcatatan.add(new ModelCatatan("Catatn7","nama saya alfi","29/12/2024"));
         modelcatatan.add(new ModelCatatan("Catatn1","nama saya hasni","29/12/2024"));
         modelcatatan.add(new ModelCatatan("Catatn2","nama saya fila","29/12/2024"));
         modelcatatan.add(new ModelCatatan("Catatn3","nama saya asya","29/12/2024"));
--- app/src/main/java/com/example/kelompok1/MainActivity.java
+++ app/src/main/java/com/example/kelompok1/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.kelompok1;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,68 @@ public class MainActivity extends AppCompatActivity {
         ImageView btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(v -> {
             finish();
+        });
+
+
+        ImageView warna1 = findViewById(R.id.warna1);
+        ImageView warna2 = findViewById(R.id.warna2);
+        ImageView warna3 = findViewById(R.id.warna3);
+        ImageView warna4 = findViewById(R.id.warna4);
+        ImageView warna5 = findViewById(R.id.warna5);
+        AtomicReference<String> background = new AtomicReference<>("");
+
+        warna1.setOnClickListener(v -> {
+            Toast.makeText(this, "Warna 1 dipilih", Toast.LENGTH_SHORT).show();
+            warna1.setImageResource(R.drawable.done);
+            warna2.setImageResource(0);
+            warna3.setImageResource(0);
+            warna4.setImageResource(0);
+            warna5.setImageResource(0);
+            background.set("#D0E8C5");
+        });
+
+
+        warna2.setOnClickListener(v -> {
+            Toast.makeText(this, "Warna 2 dipilih", Toast.LENGTH_SHORT).show();
+            warna1.setImageResource(0);
+            warna2.setImageResource(R.drawable.done);
+            warna3.setImageResource(0);
+            warna4.setImageResource(0);
+            warna5.setImageResource(0);
+            background.set("#BFECFF");
+        });
+
+
+        warna3.setOnClickListener(v -> {
+            Toast.makeText(this, "Warna 3 dipilih", Toast.LENGTH_SHORT).show();
+            warna1.setImageResource(0);
+            warna2.setImageResource(0);
+            warna3.setImageResource(R.drawable.done);
+            warna4.setImageResource(0);
+            warna5.setImageResource(0);
+            background.set("#CDC1FF");
+        });
+
+
+        warna4.setOnClickListener(v -> {
+            Toast.makeText(this, "Warna 4 dipilih", Toast.LENGTH_SHORT).show();
+            warna1.setImageResource(0);
+            warna2.setImageResource(0);
+            warna3.setImageResource(0);
+            warna4.setImageResource(R.drawable.done);
+            warna5.setImageResource(0);
+            background.set("#FFF6E3");
+        });
+
+
+        warna5.setOnClickListener(v -> {
+            Toast.makeText(this, "Warna 5 dipilih", Toast.LENGTH_SHORT).show();
+            warna1.setImageResource(0);
+            warna2.setImageResource(0);
+            warna3.setImageResource(0);
+            warna4.setImageResource(0);
+            warna5.setImageResource(R.drawable.done);
+            background.set("#FFCCEA");
         });
     }
 }
